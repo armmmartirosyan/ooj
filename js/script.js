e9ab4cf7d9e1a7096503dfc3d56b18ba66933199
ArtCar <armenmartirosyan020@gmail.com>
2022-07-04 23:22:53 +0400

    added registration

--- js/script.js
+++ js/script.js
@@ -31,24 +31,57 @@ $(".header__nav__btn").on("click", function(){
     $(".header__tools__item").toggleClass("hide");
     $("body").toggleClass("noScroll");
 });
-$("#log-in").on("click", function(){
+
+//---------------------show/hide register/login blocks---------------------
+function loginShow(){
     $(".login").toggleClass("show").removeClass("hide");
     if(document.body.offsetWidth>768){
         $("body").toggleClass("noScroll");
     }
     $(".login--wrapper").toggleClass("show").removeClass("hide");
-});
-$(".login__close").on("click", function(){
+}
+function loginHide(){
     $(".login").toggleClass("hide").removeClass("show");
     if(document.body.offsetWidth>768){
         $("body").removeClass("noScroll");
     }
     $(".login--wrapper").toggleClass("hide").removeClass("show");
+}
+function registerShow(){
+    $(".register").toggleClass("show").removeClass("hide");
+    if(document.body.offsetWidth>768){
+        $("body").toggleClass("noScroll");
+    }
+    $(".register--wrapper").toggleClass("show").removeClass("hide");
+}
+function registerHide(){
+    $(".register").toggleClass("hide").removeClass("show");
+    if(document.body.offsetWidth>768){
+        $("body").removeClass("noScroll");
+    }
+    $(".register--wrapper").toggleClass("hide").removeClass("show");
+}
+
+$("#log-in").on("click", function(){
+    loginShow();
+});
+$(".login__close").on("click", function(){
+    loginHide();
+});
+$(".login__registration__link").on("click", function(){
+    loginHide();
+    registerShow();
+});
+$(".register__registration__link").on("click", function(){
+    registerHide();
+    loginShow();
+});
+$(".register__close").on("click", function(){
+    registerHide();
 });
 
-var showPass = document.getElementById('password');
-var showPassImg = document.getElementById('password-visible');
-$("#password-visible").on("click", function(){
+//---------------------change input type---------------------
+function changeInputType(showPass, showPassImg){
     if(showPass.type==='text'){
         showPass.type='password';
         showPassImg.src = "img/icons/eye.svg";
@@ -56,8 +89,26 @@ $("#password-visible").on("click", function(){
         showPass.type='text';
         showPassImg.src = "img/icons/eye-open.svg";
     }
+}
+
+var showPass = document.getElementById('password');
+var showPassImg = document.getElementById('password-visible');
+var showPassRegister = document.getElementById('register-password');
+var showPassImgRegister = document.getElementById('register-password-visible');
+var showPassRegisterRepeat = document.getElementById('register-password-repeat');
+var showPassImgRegisterRepeat = document.getElementById('register-password-visible-repeat');
+
+$(showPassImg).on("click", function(){
+    changeInputType(showPass, showPassImg);
 });
 
+$(showPassImgRegister).on("click", function(){
+    changeInputType(showPassRegister, showPassImgRegister);
+});
+
+$(showPassImgRegisterRepeat).on("click", function(){
+    changeInputType(showPassRegisterRepeat, showPassImgRegisterRepeat);
+});
 //document.getElementById('test').setAttribute('type', 'password');
 
 //Checking information
